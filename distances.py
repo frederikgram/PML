581676a58127baa3ac07e85b113855d014ea4b35
frederikgram <frederikxyz@hotmail.com>
2018-11-23 18:04:55 +0100

    Made euclidian distance calculations work for multiple types

--- distances.py
+++ distances.py
@@ -1,3 +1,15 @@
 import numpy as np
-def euclidian(a, b):
+
+
+def euclidian(a: float or list, b: float or list) -> float or list:
+    """ Calculates the euclidian distance between ints/floats or lists of them"""
+    if isinstance(a, list):
+        a = sum(a)
+
+    if isinstance(b, list):
+         b = sum(b)
+
     return np.linalg.norm(a - b)
+
+
+print(euclidian([1, 5], [2,7]))
